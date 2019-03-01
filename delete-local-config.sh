67d4d47e8a165d27b916c2ea0c870b1bdea2138f
Karel Fiala <fiala.karel@gmail.com>
2019-03-01 22:32:13 +0100

    Fix delete-local-config.sh

--- delete-local-config.sh
+++ delete-local-config.sh
@@ -2,15 +2,21 @@
 
 set -eufo pipefail
 
+prefix="$HOME"
 src="$(dirname $(realpath $0))"
+dst="$prefix"
 
 echo "Source is: $src"
+echo "Destination is: $dst"
 
 # For each file delete actual version
-for f in $(find $src -type f | egrep -v "create-symlinks.sh|delete-local-config.sh|README.md")
+for f in $(find $src -type f | egrep -v "create-symlinks.sh|delete-local-config.sh|README.md|\.git\/")
 do
-    echo "rm $f"
+    conffile="${f#$src/}"
+    conffilename="${f##*/}"
+    conffilepath="${conffile%$conffilename}"
+    echo "rm $dst/$conffile"
     set +e
-    rm $f
+    rm $dst/$conffile
     set -e
 done
